141d162dcde14df199475f7c0e07e3911661dbea
nathankirk91 <64616413+nathankirk91@users.noreply.github.com>
2020-10-12 20:54:14 +1100

    book appointment page created with iFrame

--- src/pages/book-an-appointment.tsx
+++ src/pages/book-an-appointment.tsx
@@ -2,14 +2,38 @@ import React from "react"
 import { PageProps, Link } from "gatsby"
 
 import SEO from "../components/seo"
+import styled from "styled-components"
 
 const BookAnAppointmentPage: React.FC<PageProps> = () => (
   <>
     <SEO title="Book An Appointment" />
     <div>
-      <h2>Book An Appointment</h2>
+      <TitleContainer>
+        <h2>BOOK YOUR NEXT APPOINTMENT TODAY!</h2>
+      </TitleContainer>
+      <Container>
+        <Iframe
+          src="https://www.MyHealth1st.com.au/AppointmentWidget?practice_id=2953"
+          frameBorder={0}
+        >
+          {" "}
+        </Iframe>
+      </Container>
     </div>
   </>
 )
 
 export default BookAnAppointmentPage
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+const Container = styled.div`
+  height: 50vw;
+  min-height: 800px;
+  width: 100%;
+`
+const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+`
