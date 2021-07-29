00e778081e4b3cfacea24c5774c3813b18d60811
Nathan Kirk <nathan.kirk91@gmail.com>
2021-07-30 08:23:43 +1000

    fix: google analytics params

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,25 +21,17 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         trackingIds: [
-          process.env.GA_TRACKING_ID, // Google Analytics / GA
+          process.env.GA_TRACKING_ID,
           //"AW-CONVERSION_ID", // Google Ads / Adwords / AW
           //"DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
         gtagConfig: {
           // optimize_id: "OPT_CONTAINER_ID",
           anonymize_ip: true,
           cookie_expires: 0,
         },
-        // This object is used for configuration specific to this plugin
         pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          // respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: []//["/preview/**", "/do-not-track/me/too/"],
+          head: true,
         },
       },
     },
